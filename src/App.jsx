a1e9f7b1fe56672d8de1251fd48669e0e3f6470f
Vineet-23 <128267631+Vineet-23@users.noreply.github.com>
2024-03-28 16:13:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
 import Sidebar from './core/components/Sidebar'
 import Dashboard from './core/components/Dashboard'
@@ -7,10 +8,10 @@ function App () {
   return (
     <>
       <div className='flex justify-between items-start h-full'>
-        
+        <Router>
           <Sidebar />
           <Dashboard />
-        
+        </Router>
       </div>
     </>
   )
