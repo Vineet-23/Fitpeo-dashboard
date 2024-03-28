89930da776b0558b5f31eb95291240172543b199
Vineet-23 <128267631+Vineet-23@users.noreply.github.com>
2024-03-29 00:22:41 +0530

    Update Dashboard.jsx

--- src/core/components/Dashboard.jsx
+++ src/core/components/Dashboard.jsx
@@ -1,11 +1,12 @@
-import DashCard from './DashCard'
-import { AiOutlineDollarCircle } from 'react-icons/ai'
-import { CgLoadbarDoc } from 'react-icons/cg'
-import { BiMoney } from 'react-icons/bi'
-import { TbShoppingBag } from 'react-icons/tb'
-import Table from './Table'
-import DashNavbar from './DashNavbar'
-import ChartSection from './ChartSection'
+import React from 'react';
+import DashCard from './DashCard';
+import { AiOutlineDollarCircle } from 'react-icons/ai';
+import { CgLoadbarDoc } from 'react-icons/cg';
+import { BiMoney } from 'react-icons/bi';
+import { TbShoppingBag } from 'react-icons/tb';
+import Table from './Table';
+import DashNavbar from './DashNavbar';
+import ChartSection from './ChartSection';
 
 const Dashboard = ({ isOpen, setIsOpen }) => {
   const cardList = [
@@ -37,14 +38,19 @@ const Dashboard = ({ isOpen, setIsOpen }) => {
       amount: 89,
       rate: 11
     }
-  ]
+  ];
 
   return (
     <section className='flex justify-start flex-col h-screen w-full overflow-scroll py-4 px-8'>
       <DashNavbar isOpen={isOpen} setIsOpen={setIsOpen} />
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 sm:flex-row flex-wrap'>
         {cardList.map((card, index) => (
-          <DashCard card={card} key={index} />
+          <div
+            key={index}
+            className='m-1 p-1 text-white rounded-m transition-all duration-500 bg-gradient-to-br to-rose-500 via-black from-rose-100 bg-size-200 hover:bg-right-bottom'
+          >
+            <DashCard card={card} />
+          </div>
         ))}
       </div>
       <div className='grid grid-cols-1 md:grid-cols-3 md:gap-10'>
@@ -52,7 +58,7 @@ const Dashboard = ({ isOpen, setIsOpen }) => {
       </div>
       <Table />
     </section>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
