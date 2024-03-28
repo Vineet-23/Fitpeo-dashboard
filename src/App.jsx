d6fcc982448576a761374340b88bc54ac16ffd66
Vineet-23 <128267631+Vineet-23@users.noreply.github.com>
2024-03-28 17:56:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
+import { useState } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
 import Sidebar from './core/components/Sidebar'
 import Dashboard from './core/components/Dashboard'
 
-function App() {
-
- 
+function App () {
+  const [sidebar, setSidebar] = useState(true)
   return (
     <>
-    <div className='flex justify-between items-start h-full'>
-    <Router>
-        <Sidebar />
-        <Dashboard />
-    </Router>
-    </div>
+      <div className='flex justify-between items-start h-full'>
+        <Router>
+          <Sidebar isOpen={sidebar} setIsOpen={setSidebar} />
+          <Dashboard isOpen={sidebar} setIsOpen={setSidebar} />
+        </Router>
+      </div>
     </>
   )
 }
