688b3b256a666afdd462e559f5880bfe02930bab
Vineet-23 <128267631+Vineet-23@users.noreply.github.com>
2024-03-29 00:19:29 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-import App from './App'
+// Finding the root element in the DOM
+const rootElement = document.getElementById('root');
 
-const rootElement = document.getElementById('root')
-const root = createRoot(rootElement)
+// Creating a root for rendering React components
+const root = createRoot(rootElement);
 
+// Rendering the App component within a StrictMode
 root.render(
   <StrictMode>
     <App />
   </StrictMode>
-)
+);
