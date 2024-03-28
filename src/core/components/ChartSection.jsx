4e717a5efa88e639c93f48da7f34ab2c8d3966d3
Vineet-23 <128267631+Vineet-23@users.noreply.github.com>
2024-03-28 17:59:23 +0530

    Update ChartSection.jsx

--- src/core/components/ChartSection.jsx
+++ src/core/components/ChartSection.jsx
@@ -18,7 +18,7 @@ const ChartSection = () => {
               <select
                 id='months'
                 defaultValue='3'
-                className='bg-gray-100 text-gray-900 text-sm rounded-lg outline-none block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                className='bg-white-100 text-gray-900 text-sm rounded-lg outline-none block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
               >
                 <option className='text-left' value='3' selected>
                   Quarterly
