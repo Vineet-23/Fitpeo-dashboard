8255fa28f0c9758ebc927bcecfe3d4b8c9cda339
Vineet-23 <128267631+Vineet-23@users.noreply.github.com>
2024-03-30 02:37:01 +0530

    Update Donutchart.jsx

--- src/core/charts/Donutchart.jsx
+++ src/core/charts/Donutchart.jsx
@@ -11,11 +11,8 @@ const chartData = {
       label: '',
       data: [20, 50, 30],
       backgroundColor: ['#ec4899', '#3b82f6', '#eef2ff'],
-      borderWidth: [45, 25, 10],
-      borderColor: ['#ec4899', '#3b82f6', '#eef2ff'], 
-      borderJoinStyle: 'miter',
       borderRadius: 1,
-      spacing: 10
+      
     }
   ]
 }
