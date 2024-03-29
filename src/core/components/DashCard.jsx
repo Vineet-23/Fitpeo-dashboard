a4705d1b34b804796378d122d5b200e301104228
Vineet-23 <128267631+Vineet-23@users.noreply.github.com>
2024-03-30 04:32:27 +0530

    Update DashCard.jsx

--- src/core/components/DashCard.jsx
+++ src/core/components/DashCard.jsx
@@ -24,7 +24,7 @@ const DashCard = ({ card }) => {
           <span className={`font-bold text-${rate > 10 ? 'green' : 'red'}-700`}>
             {rate > 10 ? <FaArrowTrendUp /> : <FaArrowTrendDown />}
           </span>
-          <span className={`font-bold text-${rate > 10 ? 'green' : 'red'}-700`}>{rate}%</span>
+          <span className={`font-bold text-${rate < 10 ? 'green' : 'red'}-700`}>{rate}%</span>
           <span className="text-xs">this month</span>
         </p>
       </div>
