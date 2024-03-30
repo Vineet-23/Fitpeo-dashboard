7658ee8f5beb634fbff27389498c3d50ca246343
Vineet-23 <128267631+Vineet-23@users.noreply.github.com>
2024-03-30 13:30:25 +0530

    Update DashCard.jsx

--- src/core/components/DashCard.jsx
+++ src/core/components/DashCard.jsx
@@ -1,35 +1,30 @@
-import React from 'react'
-import { useId } from 'react'
-import { AiOutlineArrowUp } from 'react-icons/ai'
-import { FaArrowDown } from 'react-icons/fa'
+import React from 'react';
+import { useId } from 'react';
+import { FaArrowTrendUp } from "react-icons/fa6";
+import { FaArrowTrendDown } from "react-icons/fa6";
 
 const DashCard = ({ card }) => {
-  const { color, label, rate, amount, number, icon } = card
-  const uid = useId()
+  const { color, label, rate, amount, number, icon } = card;
+  const uid = useId();
 
   return (
-    <div
-      className='flex justify-evenly items-center px-2 py-4 rounded-md bg-white w-full'
-      key={uid}
-    >
-      <div
-        className={`grid place-items-center rounded-full h-[120px] w-[120px] text-6xl ${color}`}
-      >
+    <div className='flex justify-evenly items-center px-2 py-4 rounded-md bg-white w-full' key={uid}>
+      <div className={`grid place-items-center rounded-full h-[120px] w-[120px] text-6xl ${color}`}>
         {icon}
       </div>
       <div className='pl-2'>
         <p className='text-black-900 text-sm text-left'>{label}</p>
-        <p className='text-black-800 text-2xl text-left font-semibold'>
-          {amount ? `₹${amount}` : ''}
+        <p className='text-black-800 text-m text-left font-semibold'>
+          {amount ? `₹${amount}K` : ''}
         </p>
-        <p className='text-gray-800 text-2xl text-left font-semibold'>
+        <p className='text-gray-800 text-m text-left font-semibold'>
           {number}
         </p>
-        <p className='flex justify-start items-center text-sm text-left'>
-          <span className={`font-bold text-${rate > 10 ? 'green' : 'red'}-900`}>
-            {rate > 10 ? <AiOutlineArrowUp /> : <FaArrowDown />}
+        <p className='flex justify-start items-center text-xm text-left'>
+          <span className={`font-bold text-${rate > 10 ? 'green' : 'red'}-700`}>
+            {rate > 10 ? <FaArrowTrendUp /> : <FaArrowTrendDown />}
           </span>
-          <span className={`font-bold text-${rate > 10 ? 'green' : 'red'}-900`}>
+          <span className={`font-bold ${rate > 10 ? 'text-green-700' : 'text-red-700'}`}>
             {rate}%
           </span>
           <span className={`${rate > 10 ? 'text-green-700' : 'text-red-700'} text-xs`}>
@@ -38,7 +33,7 @@ const DashCard = ({ card }) => {
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashCard
+export default DashCard;
